825ad78a536264184a4d918047a7cdca4c299ec4
RG <shh-prc@qq.com>
2020-07-22 23:07:26 +0800

    3.6 doing

--- 3.6/源.cpp
+++ 3.6/源.cpp
@@ -1,3 +1,27 @@
 #pragma warning(disable : 4996) // 弃用VS的安全措施，使scanf等原生C函数可用
 #include<cstdio>
+#include<iostream>	
+#include<string>
+using namespace std;
 
+bool isPalindrome(string str) {
+	for (int i = 0; i < str.length() / 2; i++) {
+		if (str[i] != str[str.length() - 1 - i])
+			return false;
+	}
+	return true;
+}
+
+void ex_i() {
+	string str;
+	cin >> str;
+	if (isPalindrome(str))
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+}
+
+int main() {
+	ex_i();
+	return 0;
+}
