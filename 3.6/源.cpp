b03d4dff002cb368b71caa2fdaa8a80a38b044d6
RG <shh-prc@qq.com>
2020-07-22 23:09:00 +0800

    3.6 done

--- 3.6/源.cpp
+++ 3.6/源.cpp
@@ -4,6 +4,7 @@
 #include<string>
 using namespace std;
 
+// 问题 I: 【字符串】回文串
 bool isPalindrome(string str) {
 	for (int i = 0; i < str.length() / 2; i++) {
 		if (str[i] != str[str.length() - 1 - i])
