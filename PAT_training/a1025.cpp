df00714979fb0b031de40a7decf0218c240694d6
RG <shh-prc@qq.com>
2020-07-31 22:50:01 +0800

    a1025 doing

--- PAT_training/a1025.cpp
+++ PAT_training/a1025.cpp
@@ -10,8 +10,27 @@ struct Student
 	string id;
 	int location;
 	int grade;
+	int local_rank;//可以不设置总排名，总排名通过整体数组排序得到
 };
 
+inline void selectSort(Student stu[], int amount) {
+	for (int i = 0; i < amount; i++) {
+		int k = i;
+		for (int j = i; j < amount; j++) {
+			if (stu[j].grade > stu[k].grade) {
+				k = j;
+			}
+			else if (stu[j].grade == stu[k].grade && stu[j].id < stu[k].id) {
+				// 分数相同按照id先后排序（升序），思考一个问题 c++ string可以直接比较大小吗
+				k = j;
+			}
+		}
+		// 思考一个问题，结构体可以直接赋值吗？
+		Student tmp = stu[i];
+		stu[i] = stu[k];
+		stu[k] = tmp;
+	}
+}
 
 inline void test() {
 	return;
