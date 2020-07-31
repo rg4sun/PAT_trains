e5ed8890dc58888ad6657b21436a329171a3b5ca
RG <shh-prc@qq.com>
2020-07-31 22:27:39 +0800

    a1025 doing

--- PAT_training/a1025.cpp
+++ PAT_training/a1025.cpp
@@ -1,5 +1,17 @@
 #pragma warning(disable : 4996) // 弃用VS的安全措施，使scanf等原生C函数可用
 #include<cstdio>
+#include<string>
+#include<iostream>
+using namespace std;
+
+struct Student
+{
+	//int id; int存储不了 13位的数字
+	string id;
+	int location;
+	int grade;
+};
+
 
 inline void test() {
 	return;
