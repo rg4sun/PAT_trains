335cf7198cad889940e5fed2a3af942131ddd76c
RG <shh-prc@qq.com>
2020-08-01 16:27:43 +0800

    把id改成char数组就可以了

--- PAT_training/a1025.cpp
+++ PAT_training/a1025.cpp
@@ -7,10 +7,12 @@ using namespace std;
 struct Student
 {
 	//int id; int存储不了 13位的数字
-	string id;
+	//string id; // 之前出现一些乱码问题，改成char
+	char id[15];
 	int location;
 	int grade;
-	int local_rank;//可以不设置总排名，总排名通过整体数组排序得到
+	int local_rank;//可以不设置总排名，总排名通过整体数组排序得到(但是并列排名就还要处理)
+	int total_rank;//所以还是设置一个总排名吧。，。
 };
 
 inline void selectSort(Student stu[], int amount) {
@@ -20,13 +22,14 @@ inline void selectSort(Student stu[], int amount) {
 			if (stu[j].grade > stu[k].grade) {
 				k = j;
 			}
-			else if (stu[j].grade == stu[k].grade && stu[j].id < stu[k].id) {
+			//else if (stu[j].grade == stu[k].grade && stu[j].id < stu[k].id) {
 				// 分数相同按照id先后排序（升序），思考一个问题 c++ string可以直接比较大小吗
 				// C++ 可以直接比较
+			else if (stu[j].grade == stu[k].grade && strcmp(stu[j].id, stu[k].id) < 0) {
 				k = j;
 			}
 		}
-		// 思考一个问题，结构体可以直接赋值吗？
+		// 思考一个问题，结构体可以直接赋值吗？可以
 		Student tmp = stu[i];
 		stu[i] = stu[k];
 		stu[k] = tmp;
@@ -36,6 +39,33 @@ inline void selectSort(Student stu[], int amount) {
 inline void showStu(Student stu[], int amount) {
 	for (int i = 0; i < amount; i++) {
 		cout << stu[i].id << ' ' << stu[i].grade << ' ' << stu[i].location << ' ' << stu[i].local_rank << endl;
+	}
+}
+
+inline void calcRank(Student stu[], int amount, bool localOrTotal) {
+	if (localOrTotal) {
+		stu[0].local_rank = 1;
+		for (int i = 1; i < amount; i++) {
+			// 考场排名计算
+			if (stu[i].grade == stu[i - 1].grade) {// 分数与上一名一样则排名取上一位
+				stu[i].local_rank = stu[i - 1].local_rank;
+			}
+			else {// 不一样则排名就是当前序号
+				stu[i].local_rank = i;
+			}
+		}
+	}
+	else {
+		stu[0].total_rank = 1;
+		for (int i = 1; i < amount; i++) {
+			// 总排名计算
+			if (stu[i].grade == stu[i - 1].grade) {// 分数与上一名一样则排名取上一位
+				stu[i].total_rank = stu[i - 1].total_rank;
+			}
+			else {// 不一样则排名就是当前序号
+				stu[i].total_rank = i;
+			}
+		}
 	}
 }
 
@@ -49,13 +79,18 @@ inline void test() {
 		int k; // 考场中考生数量
 		scanf("%d", &k);
 		for (int j = 0; j < k; j++) {
-			cin >> stu[amount].id;
-			scanf("%d", &stu[amount].grade);
+			/*cin >> stu[amount].id;*/
+			scanf("%s %d", stu[amount].id, &stu[amount].grade);
 			stu[amount].location = i;
 			amount++;
 		}
+		// 这里完成了一个考场的数据录入，在此马上对这个考场进行排序
+		selectSort(stu + amount - k, k); // 这里是 指针头的计算，每个考场的第一个考生地址
+		calcRank(stu + amount - k, k, true);// 计算考场排名
+
 	}
-	selectSort(stu, amount);
+	selectSort(stu, amount);// 整体排序
+	calcRank(stu, amount, false);// 计算总排名
 	showStu(stu, amount);
 
 	return;
