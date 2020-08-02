c8f6a4d0f7939f1aad95626465ff3567461aafc9
RG <shh-prc@qq.com>
2020-08-02 15:38:04 +0800

    ex_a doing

---
+++ 4.2/源.cpp
@@ -0,0 +1,30 @@
+#pragma warning(disable : 4996) // 弃用VS的安全措施，使scanf等原生C函数可用
+#include<cstdio>
+#include<cstdlib>
+
+//问题 A: 谁是你的潜在朋友
+int bookTable[210] = { 0 }; // 书本编号哈希表
+void ex_a() {
+	int n, m;
+	scanf("%d %d", &n, &m);
+	int* buddy = (int*)malloc(n * sizeof(int));//记录每个人喜欢哪本书
+	for (int i = 0; i < n; i++) {
+		scanf("%d", buddy + i);
+		bookTable[buddy[i]]++;
+	}
+	for (int i = 0; i < n; i++) {
+		if (bookTable[buddy[i]] == 0)
+			printf("BeiJu\n");
+		else
+			printf("%d\n", bookTable[buddy[i]]);
+	}
+}
+
+
+int main()
+{
+	ex_a();
+
+	return 0;
+}
+
