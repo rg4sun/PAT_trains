967dc04fa940a7689bf882a41a94f2cfd92bfccb
RG <shh-prc@qq.com>
2020-08-08 21:44:01 +0800

    问题找到了，是count重复定义的原因，详细见调试文件4.3_A_queen4debug

--- 4.3/源.cpp
+++ 4.3/源.cpp
@@ -4,19 +4,17 @@
 #include<cmath>
 
 // 问题 D: 八皇后
-extern int count=0, p[9], hashTable[9] = { false }; // 由于要取到8，并且从1开始，所以长度设为9
-#include "3queens4debug.cpp"
-void queen8(int index, int order, int count) {
+int count=0, p[9], hashTable[9] = { false }; // 由于要取到8，并且从1开始，所以长度设为9
+void queen8(int index, int order) {
 	if (index == 9) {
-		//count++; // count更新有问题，放这里的时候，一直是1
-		if (1) { // 如果是需要输出的皇后串序号
-			printf("the %d queen sequence:\n", count);
+		count++; // count更新有问题，放这里的时候，一直是1,找到问题了，是count重复定义的原因，见调试项目
+		if (count == order) { // 如果是需要输出的皇后串序号
+			//printf("the %d queen sequence:\n", count);
 			for (int i = 1; i < 9; i++) {
 				printf("%d", p[i]);
 			}
 			printf("\n");
 		}
-		//count++;
 		return;
 	}
 	for (int x = 1; x <= 8; x++) {
@@ -33,7 +31,7 @@ void queen8(int index, int order, int count) {
 			if (notCollision) {// 不冲撞，则当前x可以存入序列
 				p[index] = x;
 				hashTable[x] = true;
-				queen8(index + 1, order, count); // 递归计算下一列
+				queen8(index + 1, order); // 递归计算下一列
 				hashTable[x] = false;// 本行递归完毕，重置本行哈希表
 			}
 		}
@@ -46,7 +44,7 @@ void ex_d() {
 		for (int i = 0; i < n; i++) {
 			int order;
 			scanf("%d", &order);
-			queen8(1, order, count); // 输出当前序号的 皇后串
+			queen8(1, order); // 输出当前序号的 皇后串
 			for (i = 0; i < 9; i++) { // 哈希表整个重置
 				hashTable[i] = false;
 			}
@@ -58,11 +56,11 @@ void ex_d() {
 void test() {
 	int order;
 	scanf("%d", &order);
-	queen8(1, order, count); // 输出当前序号的 皇后串
+	queen8(1, order); // 输出当前序号的 皇后串
 }
 
 int main()
 {
-	queen4debug();
+	test();
 	return 0;
 }
